e92ab04319b4796bee9d48439e59ddcb24a32e65
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-10-28 19:06:04 +0000

    made a simple add corection

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Returns start and end indices for items on a specified page."""
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = page + page_size
     return (start_index, end_index)
