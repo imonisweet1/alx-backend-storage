0410a4af08a13e4b823dd03e5e9eb4db27e6e95c
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-10-28 11:24:58 +0000

    0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Helper function for pagination index calculation.
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
