86a8a27b18470508045486ca45e5da37eacde380
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2024-10-28 11:53:54 +0000

    made more corrections to task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,4 +10,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return start_index, end_index
+
+    return (start_index, end_index)
