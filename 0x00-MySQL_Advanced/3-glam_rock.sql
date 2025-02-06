66a7d80cbd8b480fd2ff2cfd3afa0bb8f156fdee
Imonivwerha Isaac <isaacimonivwerha@gmail.com>
2025-02-06 16:27:34 +0100

    tasks 002

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,15 @@
+-- List bands with Glam rock as their main style
+-- ranked by their longevity in desending order
+SELECT 
+    band_name, 
+    CASE 
+        WHEN split IS NOT NULL THEN split - formed 
+        ELSE 2022 - formed 
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    style LIKE '%Glam%'
+ORDER BY 
+    lifespan DESC;
+
